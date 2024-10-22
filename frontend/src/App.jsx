10534133bccaa41e0d6bf225c3a0f226be5d01df
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-22 18:37:50 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div className="tex-4xl bg-red-700 p-4 text-center">App</div>;
+  return (
+    <div className="tex-4xl bg-red-700 p-4 text-center">
+      This is the base application
+    </div>
+  );
 };
 
 export default App;
