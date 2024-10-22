eab6551cd1c7af9fa6ceee96b83a27161a9ac7b8
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-22 18:44:00 +0530

    start frontend project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="tex-4xl bg-red-700 p-4 text-center">
-      This is the base application
+      here we start our frontend
     </div>
   );
 };
