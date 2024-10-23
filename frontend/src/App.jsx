a2af49b35a7a2411ede3fb8ce4188d590d72628c
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-23 11:44:52 +0530

    add component and login information and install redux-toolkit in this project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/homepage/Home";
+import Login from "./components/LoginSection/Login";
+import Verify_otp from "./components/LoginSection/Verify_otp";
+import Forgot_password from "./components/LoginSection/Forgot_password";
+import Reset_password from "./components/LoginSection/Reset_password";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />, // later we apply a protected route here
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/verify-otp",
+      element: <Verify_otp />,
+    },
+    {
+      path: "/forgot-password",
+      element: <Forgot_password />,
+    },
+    {
+      path: "/reset-password",
+      element: <Reset_password />,
+    },
+  ]);
   return (
-    <div className="text-2xl bg-red-500">
-      here we start the frontend project with react and tailwind css
+    <div>
+      <div className="text-2xl bg-red-500">
+        here we start the frontend project with react and tailwind css
+      </div>
+      <RouterProvider router={router} />
     </div>
   );
 };
---
+++ frontend/src/components/LoginSection/Verify_otp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Verify_otp = () => {
+  return <div>Verify_otp</div>;
+};
+
+export default Verify_otp;
