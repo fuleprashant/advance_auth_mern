6aed29f0758b47aaace263887610a90761fdd7ff
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-23 17:24:22 +0530

    add react-hook-form adn schemas for the validation

--- frontend/src/components/LoginSection/Login.jsx
+++ frontend/src/components/LoginSection/Login.jsx
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
 import logo from "../../assets/download (1).png";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+
+  const signInSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("password must be required"),
+  });
+
+  const signUpSchema = yup.object().shape({
+    name: yup.string().required("name is required"),
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("password must be required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { error },
+  } = useForm({
+    resolver: yupResolver(isSignUp ? signUpSchema : signInSchema),
+  });
 
   return (
     <div className="flex min-h-full flex-1 justify-center px-6 py-12 lg:px-8 mt-32">
@@ -17,6 +52,7 @@ const Login = () => {
           {isSignUp ? "Create your account" : "sign in to your account"}
         </h2>
       </div>
+      {/* here we start the react-hook-form */}
     </div>
   );
 };
