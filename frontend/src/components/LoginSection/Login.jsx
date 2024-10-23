1e4015757dad795e1aad0237f1c7a9d75abb4a82
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-23 16:08:11 +0530

    add header part of the  login page

--- frontend/src/components/LoginSection/Login.jsx
+++ frontend/src/components/LoginSection/Login.jsx
@@ -7,12 +7,15 @@ const Login = () => {
 
   return (
     <div className="flex min-h-full flex-1 justify-center px-6 py-12 lg:px-8 mt-32">
-      <div className="flex flex-col justify-center w-full px-4 py-10 md:px-0 space-y-6 shadow-lg rounded-l-lg">
+      <div className="flex flex-col justify-center w-[1254px] px-4 py-10 md:px-0 space-y-6 shadow-lg rounded-l-lg ">
         <img
           src={logo}
           alt="your company logo"
           className="mx-auto h-10 w-auto"
         />
+        <h2 className="text-center mt-10 font-bold text-2xl text-gray-900">
+          {isSignUp ? "Create your account" : "sign in to your account"}
+        </h2>
       </div>
     </div>
   );
