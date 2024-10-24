e6aa429c81580188d3c36328ea9f99dffc30dd5e
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-24 12:31:38 +0530

    add api intigration and the env variable isue solve

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,26 @@ import dotenv from "dotenv";
 import db from "./database/db.js";
 import router from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 db();
-app.use(express.json());
+
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173", // Your frontend URL
+  credentials: true, // Allow credentials
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+};
+
+// Use CORS middleware
+app.use(cors(corsOptions));
+
+// Body parser middleware
+app.use(express.json());
 app.use("/user", router);
 app.get("/", (req, res) => {
   res.send("here our server has been start ");
@@ -21,4 +33,3 @@ app.listen(port, () => {
     `the server has been start on the port number http://localhost:${port}`
   );
 });
-
